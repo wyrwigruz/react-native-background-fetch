a1a97593195cc549094bd0f39a4304f9666dbf09
Paweł Pęcikiewicz <pawel@pecikiewicz.com>
2018-02-16 15:57:35 +0100

    fix in background

--- android/src/main/java/com/transistorsoft/rnbackgroundfetch/HeadlessBroadcastReceiver.java
+++ android/src/main/java/com/transistorsoft/rnbackgroundfetch/HeadlessBroadcastReceiver.java
@@ -1,5 +1,6 @@
 package com.transistorsoft.rnbackgroundfetch;
 
+import android.app.ActivityManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -7,18 +8,34 @@ import android.util.Log;
 
 import com.transistorsoft.tsbackgroundfetch.BackgroundFetch;
 
-/**
- * Created by chris on 2018-01-20.
- */
+import java.util.List;
 
 public class HeadlessBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         BackgroundFetch adapter = BackgroundFetch.getInstance(context.getApplicationContext());
-        if (adapter.isMainActivityActive()) {
+        if (isAppOnForeground(context)) {
             return;
         }
         Log.d(BackgroundFetch.TAG, "HeadlessBroadcastReceiver onReceive");
         new HeadlessTask(context.getApplicationContext());
+    }
+
+    private boolean isAppOnForeground(Context context) {
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> appProcesses =
+                activityManager.getRunningAppProcesses();
+        if (appProcesses == null) {
+            return false;
+        }
+        final String packageName = context.getPackageName();
+        for (ActivityManager.RunningAppProcessInfo appProcess : appProcesses) {
+            if (appProcess.importance ==
+                    ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND &&
+                    appProcess.processName.equals(packageName)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
